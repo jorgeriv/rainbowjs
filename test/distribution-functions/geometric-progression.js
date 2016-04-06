e051b05824c4739ee03b92a44b6e7d824e880b59
Jorge Rivera <jorgeriv@gmail.com>
2016-04-06 10:47:19 -0500

    Add unit tests for geometric-progression.js

---
+++ test/distribution-functions/geometric-progression.js
@@ -0,0 +1,35 @@
+const requirejs = require('../requirejs');
+const geometricProgression = requirejs('distribution-functions/geometric-progression');
+
+describe('Geometric progressions', ()=>{'use strict';
+
+it('should generate an array of one value by default', ()=>{
+  let result = geometricProgression();
+  expect(result.length).toBe(1);
+  expect(result[0]).toBe(1);
+});
+
+it('should create a geometric progression with 3 values', ()=>{
+  let iterations = 3,
+  result = geometricProgression(iterations);
+  expect(result.length).toBe(iterations);
+  expect(result[2]).toEqual(Math.pow(0.5, iterations -1));
+});
+
+it('should return only the last value', ()=>{
+  let iterations = 5,
+      ratio = 0.2,
+      result = geometricProgression(iterations, ratio, true);
+  expect(typeof result).toEqual('number');
+  expect(result).toEqual(Math.pow(0.2, iterations - 1));
+});
+
+it('should return a geometric progression (array) with 8 values', ()=>{
+  let iterations = 8,
+      ratio = 2,
+      result = geometricProgression(iterations, ratio);
+  expect(typeof result).toEqual('object');
+  expect(result.length).toBeDefined();
+  expect(result[iterations - 1]).toEqual(Math.pow(ratio, iterations -1));
+});
+});
--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
 'use strict';
 
 require('./color');
+require('./distribution-functions/geometric-progression');
