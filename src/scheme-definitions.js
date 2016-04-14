6b6e162f8867a91d4482c9ff922556ab59ae282c
Jorge Rivera <jorgeriv@gmail.com>
2016-04-14 15:37:17 -0500

    add scheme-definitions

---
+++ src/scheme-definitions.js
@@ -0,0 +1,47 @@
+'use strict';
+let schemePresets = {
+  analogous: {
+    count: 3,
+    fn: 'analogous',
+    options: {
+
+    }
+  },
+  complementary: {
+    count: 1,
+    fn: 'complement',
+    options: {
+
+    }
+  },
+  splitComplementary: {
+    count: 3,
+    fn: 'complement',
+    options: {
+
+    }
+  },
+  triadic: {
+    count: 3,
+    fn: 'equidistant',
+    options: {
+
+    }
+  },
+  squere: {
+    count: 4,
+    fn: 'equidistant',
+    options: {
+
+    }
+  },
+  rectangle: {
+    count: 4,
+    fn: 'rotate',
+    options: {
+
+    }
+  },
+};
+
+module.exports = schemePresets;
