94c83c224630aca79f4995872f542dcf6ff8d0a0
Jorge Rivera <jorgeriv@gmail.com>
2016-04-22 14:39:40 -0500

    Refactor schema tests
    Group related tests to nested describe functions

--- test/scheme.js
+++ test/scheme.js
@@ -3,76 +3,106 @@ const Color = require('../src/color');
 const Scheme = require('../src/scheme');
 
 describe('Scheme', ()=>{
-let scheme, clone;
 
-it('should create an empty scheme', ()=>{
-  scheme = new Scheme();
-  expect(scheme instanceof Object).toBe(true);
-  expect(scheme.colors[0].base).toEqual(new Color());
-  expect(scheme.name).toBeUndefined();
-});
-
-it('should name an scheme', ()=>{
-  let name = 'test';
-  scheme.setName(name);
-  expect(scheme.name).toBeDefined();
-  expect(scheme.getName()).toBe(name);
-});
-
-it('should reset scheme to initial default values', ()=>{
-  scheme.reset();
-  expect(scheme.name).toBeUndefined();
-});
-
-it('should generate secundary colors given a harmony function', ()=>{
-  scheme.configure('triadic');
-  scheme.generate();
-  expect(scheme.colors).toBeDefined();
-  expect(scheme.colors instanceof Array).toBe(true);
-  expect(scheme.colors.length).toBe(3);
-});
-
-it('should generate 3 shades for each color in the scheme', ()=>{
-  scheme.generateShades(3);
-  scheme.colors.forEach((color)=>{
-    expect(color.shades).toBeDefined();
-    expect(color.shades instanceof Array).toBe(true);
-    expect(color.shades.length).toBe(3);
+describe('instantiation', ()=>{
+  it('should create an empty scheme', ()=>{
+    let scheme = new Scheme();
+    expect(scheme instanceof Object).toBe(true);
+    expect(scheme.colors[0].base).toEqual(new Color());
+    expect(scheme.name).toBeUndefined();
   });
-});
 
-it('should generate 3 tints for each color in the scheme', ()=>{
-  scheme.generateTints(3);
-  scheme.colors.forEach((color)=>{
-    expect(color.tints).toBeDefined();
-    expect(color.tints instanceof Array).toBe(true);
-    expect(color.tints.length).toBe(3);
+  it('should create and initializate an scheme', ()=>{
+    let scheme, config;
+    config = {
+      name: 'test',
+      colors:[{base: (new Color()).toJSON()}]
+    };
+    scheme = new Scheme(config);
+    expect(scheme.name).toBe(config.name);
+    expect(scheme.colors).toBeDefined();
+    expect(scheme.colors[0].base).toEqual((new Color()).toJSON());
   });
-});
+});// <<< instantiation
 
-it('should generate 3 tones for each color in the scheme', ()=>{
-  scheme.generateTones(3);
-  scheme.colors.forEach((color)=>{
-    expect(color.tones).toBeDefined();
-    expect(color.tones instanceof Array).toBe(true);
-    expect(color.tones.length).toBe(3);
+describe('populate colors methods', ()=>{
+  it('should generate secundary colors given a harmony function', ()=>{
+    let scheme = new Scheme();
+    scheme.configure('triadic');
+    scheme.generate();
+    expect(scheme.colors).toBeDefined();
+    expect(scheme.colors instanceof Array).toBe(true);
+    expect(scheme.colors.length).toBe(3);
   });
-});
+
+  it('should generate 3 shades for each color in the scheme', ()=>{
+    let scheme = new Scheme();
+    scheme.generateShades(3);
+    scheme.colors.forEach((color)=>{
+      expect(color.shades).toBeDefined();
+      expect(color.shades instanceof Array).toBe(true);
+      expect(color.shades.length).toBe(3);
+    });
+  });
+
+  it('should generate 3 tints for each color in the scheme', ()=>{
+    let scheme = new Scheme();
+    scheme.generateTints(3);
+    scheme.colors.forEach((color)=>{
+      expect(color.tints).toBeDefined();
+      expect(color.tints instanceof Array).toBe(true);
+      expect(color.tints.length).toBe(3);
+    });
+  });
+
+  it('should generate 3 tones for each color in the scheme', ()=>{
+    let scheme = new Scheme();
+    scheme.generateTones(3);
+    scheme.colors.forEach((color)=>{
+      expect(color.tones).toBeDefined();
+      expect(color.tones instanceof Array).toBe(true);
+      expect(color.tones.length).toBe(3);
+    });
+  });
+});// <<< populate colors methods
+
+describe('modifiers', ()=>{
+
+  it('should name an scheme', ()=>{
+    let scheme = new Scheme(),
+        name = 'test';
+    scheme.setName(name);
+    expect(scheme.name).toBeDefined();
+    expect(scheme.getName()).toBe(name);
+  });
+
+  it('should reset scheme to initial default values', ()=>{
+    let scheme = new Scheme({name: 'test'});
+    scheme.reset();
+    expect(scheme.name).toBeUndefined();
+  });
+
+  it('should rotate hue wheel of all colors', ()=>{
+    let scheme = new Scheme(),
+        clone = scheme.clone(),
+        angle = 0.5,
+        hue = clone.colors[0].base.rotateHueWheel(angle).getHSV().h;
+
+    scheme.rotate(angle);
+    expect(hue)
+      .toEqual(scheme.colors[0].base.getHSV().h);
+  });
+});// <<< modifiers
 
 it('should clone the scheme',()=>{
-  clone = scheme.clone();
-  expect(clone.name).toBe(scheme.name);
-  expect(clone.colors.length).toEqual(scheme.colors.length);
-  expect(clone.colors[0]).toEqual(scheme.colors[0]);
+  let scheme = new Scheme(),
+      clone = scheme.clone();
+
+  expect(clone).toEqual(scheme);
+  expect(clone).not.toBe(scheme);
 });
 
-it('should rotate hue wheel of all colors', ()=>{
-  let angle = 0.5,
-      hue = clone.colors[0].base.rotateHueWheel(angle).getHSV().h;
-  scheme.rotate(angle);
-  expect(hue)
-    .toEqual(scheme.colors[0].base.getHSV().h);
-});
+
 
 it('should return a JSON', ()=>{
   let scheme = new Scheme(),
@@ -85,4 +115,5 @@ it('should return a JSON string reprecenting the object', ()=>{
   json = JSON.stringify(scheme);
   expect(json).toBe(JSON.stringify({name:undefined,colors:[{base: new Color()}]}));
 });
+
 });
