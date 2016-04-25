ac467fc695efbd5d1d9a03a8a7ef6eae6863d2de
Jorge Rivera <jorgeriv@gmail.com>
2016-04-24 20:58:00 -0500

    add test task

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -2,8 +2,9 @@
 
 const gulp = require('gulp');
 const jasmine = require('gulp-jasmine');
-
-gulp.task('test', () => {
-gulp.src('test/index.js')
-.pipe(jasmine());
-});
+module.exports = ()=>{
+  return gulp.task('test', () => {
+    gulp.src('test/index.js')
+    .pipe(jasmine());
+  });
+};
