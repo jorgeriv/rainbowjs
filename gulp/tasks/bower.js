fdd74afe3d3afed239aed89f87fd4978ef4fb442
Jorge Rivera <jorgeriv@gmail.com>
2016-04-27 12:46:32 -0500

    add creation of bower.json file

--- gulp/tasks/bower.js
+++ gulp/tasks/bower.js
@@ -4,17 +4,33 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 const fs = require('fs');
 
-function getPackageJsonVersion () {
   // We parse the json file instead of using require because require caches
   // multiple calls so the version number won't be updated
-  return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
-}
+let pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+let bowerjson = JSON.stringify({
+  'name': pkg.name,
+  'description': pkg.description,
+  'main': 'rainbow.js',
+  'authors': [
+    'Jorge Rivera <jorgeriv@gmail.com>'
+  ],
+  'license': pkg.license,
+  'keywords': pkg.keywords,
+  'homepage': pkg.homepage,
+  'moduleType': [
+    'globals',
+    'node'
+  ],
+  'ignore': []
+}, null, 2);
 
 module.exports = ()=>{
-  let ver = getPackageJsonVersion();
   return gulp.task('bower', ['browserify'], ()=>{
-    gulp.src(`dist/rainbow-${ver}.js`)
+    gulp.src(`dist/rainbow-${pkg.version}.js`)
     .pipe(rename({basename:'rainbow', extname:'.js'}))
     .pipe(gulp.dest('bower-rainbow'));
+
+    // Create bower.json
+    fs.writeFile('./bower-rainbow/bower.json', bowerjson, 'utf-8');
   });
 };
